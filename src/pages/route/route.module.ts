2d31cd640ca2048c20eecce21590c3cac8df0d40
ahmed hayat <ahmed006@yahoo.com>
2018-06-12 16:33:49 +0500

    compiled for route 222

--- src/pages/route/route.module.ts
+++ src/pages/route/route.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RoutePage } from './route';
-import {Geolocation} from '@ionic-native/geolocation'
 
 @NgModule({
   declarations: [
